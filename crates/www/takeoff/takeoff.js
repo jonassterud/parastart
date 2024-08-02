ff537ad4d84ad7762068103d1e28a0044b57f023
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2024-08-02 16:57:13 +0000

    feat: progress on Windy iframe

--- crates/www/takeoff/takeoff.js
+++ crates/www/takeoff/takeoff.js
@@ -29,6 +29,7 @@ async function load_takeoff(id) {
  */
 
 async function display_takeoff(takeoff) {
+    // Get HTML elements
     const e_name = document.getElementById("name");
     const e_region = document.getElementById("region");
     const e_image = document.getElementById("image");
@@ -36,11 +37,14 @@ async function display_takeoff(takeoff) {
     const e_updated = document.getElementById("updated");
     const e_created = document.getElementById("created");
     const e_source_url = document.getElementById("source-url");
+    const e_windy_iframe = document.getElementById("windy-iframe");
 
-    if ([e_name, e_region, e_image, e_description, e_updated, e_created, e_source_url].includes(null)) {
+    // Guard against missing elements
+    if ([e_name, e_region, e_image, e_description, e_updated, e_created, e_source_url, e_windy_iframe].includes(null)) {
         throw new Error("failed finding one or more HTML elements");
     }
 
+    // Set text contents
     e_name.innerText = takeoff.name;
     e_region.innerText = takeoff.region;
     e_description.innerText = takeoff.description;
@@ -48,14 +52,24 @@ async function display_takeoff(takeoff) {
     e_created.innerText = takeoff.created;
     e_source_url.setAttribute("href", takeoff.source_url);
 
+    // Create image
     if (takeoff.image !== null) {
         const image_base64 = btoa(String.fromCharCode.apply(null, new Uint8Array(takeoff.image)));
         e_image.src = `data:image/png;base64,${image_base64}`;
         e_image.removeAttribute("hidden");
     }
 
+    // Configure compass
     takeoff.wind_dirs.forEach((dir) => {
         const dir_e = document.querySelector(`#compass #${dir}`);
         dir_e.removeAttribute("hidden");
     });
+
+    // Configure Windy frame
+    e_windy_iframe.src = `https://embed.windy.com/embed.html?type=map&location=coordinates
+    &metricRain=mm&metricTemp=°C&metricWind=m/s&zoom=5&overlay=wind&product=ecmwf&level=200m
+    &lat=${takeoff.latitude}&lon=${takeoff.longitude}&detailLat=${takeoff.latitude}&detailLon=${takeoff.longitude}
+    &detail=true&message=true`;
+
+    console.log(takeoff);
 }
