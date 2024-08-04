30873252b18f2a30f638fe9d2f2eda79a0fff2f3
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2024-08-04 19:16:10 +0000

    refactor: formatting, etc.

--- crates/www/takeoff/takeoff.js
+++ crates/www/takeoff/takeoff.js
@@ -4,22 +4,11 @@ try {
     const params = new URL(document.location.toString()).searchParams;
     const id = params.get("id");
 
-    if (id !== null) {
-        load_takeoff(id).then(display_takeoff);
-    }
+    if (id !== null) throw new Error("missing id parameter");
+
+    fetch_takeoff(id).then(display_takeoff);
 } catch (error) {
     console.error(error);
-}
-
-/**
- * Load takeoff.
- * 
- * @param {Number} id - Takeoff ID.
- */
-async function load_takeoff(id) {
-    const takeoff = await fetch_takeoff(id);
-
-    return takeoff;
 }
 
 /**
@@ -50,9 +39,7 @@ async function display_takeoff(takeoff) {
         e_readable_height, e_altitude, e_altitude_diff
     ];
 
-    if (required_elements.includes(null)) {
-        throw new Error("failed finding one or more HTML elements");
-    }
+    if (required_elements.includes(null)) throw new Error("missing HTML elements");
 
     // Set text contents
     e_name.innerText = takeoff.name;
@@ -74,16 +61,16 @@ async function display_takeoff(takeoff) {
 
     // Configure compass
     takeoff.wind_dirs.forEach((dir) => {
-        const dir_e = document.querySelector(`#compass #${dir}`);
-        dir_e.removeAttribute("hidden");
+        const e_dir = document.querySelector(`#compass #${dir}`);
+        e_dir.removeAttribute("hidden");
     });
 
     // Set Windy iframe function
-    const set_windy_iframe = (latitude, longitude, height) => {
+    const set_windy_iframe = (lat, lon, h) => {
         e_windy_iframe.src = `https://embed.windy.com/embed.html?
         type=map&location=coordinates&metricRain=mm&metricTemp=°C&metricWind=m/s
-        &zoom=5&overlay=wind&product=ecmwf&level=${height}&lat=${latitude}&lon=${longitude}
-        &detailLat=${latitude}&detailLon=${longitude}&detail=true&message=true`;    
+        &zoom=5&overlay=wind&product=ecmwf&level=${h}&lat=${lat}&lon=${lon}
+        &detailLat=${lat}&detailLon=${lon}&detail=true&message=true`;    
     };
 
     // Synchronize height slider and Windy iframe function
@@ -93,8 +80,6 @@ async function display_takeoff(takeoff) {
 
         set_windy_iframe(takeoff.latitude, takeoff.longitude, windy_height);
         e_readable_height.innerText = readable_height;
-
-        console.log(element);
     };
 
     // Configure Windy height slider
@@ -103,7 +88,7 @@ async function display_takeoff(takeoff) {
     const readable_heights = ["Surface", "100 m", "600 m", "750 m", "900 m", "1500 m", "2000 m", "3000 m", "4200 m", "5500 m", "7000 m", "9000 m", "10 km", "11,7 km", "13,5 km", "30 km"]
     e_windy_height.addEventListener("change", synchronize_windy_slider);
 
-    // Set best initial height for height slider
+    // Set closest initial height for height slider
     const initial_number_height = number_heights.reduce((prev, curr) => (Math.abs(curr - takeoff.altitude) < Math.abs(prev - takeoff.altitude) ? curr : prev));
     e_windy_height.value = number_heights.indexOf(initial_number_height);
     synchronize_windy_slider();
