ca19b8ac499bcddd5a9822c59231e6c824595385
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2024-08-08 21:32:44 +0000

    feat: progress on list + search

--- crates/www/scripts/list.js
+++ crates/www/scripts/list.js
@@ -1,25 +1,81 @@
 "use strict"
 
-async function fill_results() {
+window.onload = () => {
+    try {
+        fill_results_table().then(handle_sorting);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/**
+ * Fill the "results-table" with takeoffs.
+ * 
+ * @returns An array of inserted rows.
+ */
+async function fill_results_table() {
     // Get HTML elements
-    const e_results_container = document.getElementById("results-container");
+    const e_results_content = document.getElementById("results-content");
 
     // Guard against missing elements
-    const required_elements = [e_results_container];
+    const required_elements = [e_results_content];
     if (required_elements.includes(null)) throw new Error("missing HTML elements");
 
     // Fetch (all) takeoffs (if missing any)
+    let takeoffs = [];
     const hash = await fetch_takeoffs(undefined, undefined, undefined, ["id"])
         .then((res) => res.map((v) => v.id).join(''))
         .then((s => cyrb53(s).toString()));
 
     if (hash !== window.localStorage.getItem("hash")) {
-        const takeoffs = await fetch_takeoffs(undefined, undefined, undefined, ["id", "name", "region", "latitude", "longitude"]);
+        takeoffs = await fetch_takeoffs(undefined, undefined, undefined, ["id", "name", "description", "region", "latitude", "longitude"]);
         window.localStorage.setItem("takeoffs", JSON.stringify(takeoffs));
         window.localStorage.setItem("hash", cyrb53(takeoffs.map((v) => v.id).join('')).toString());
-        console.log("fetching")
+    } else {
+        takeoffs = JSON.parse(window.localStorage.getItem("takeoffs"));
     }
 
+    // Insert takeoffs
+    const out = takeoffs.map((takeoff) => {
+        const e_takeoff_container = document.createElement("div");
+        const e_takeoff_name = document.createElement("span");
+        const e_takeoff_description = document.createElement("span");
+        const e_takeoff_region = document.createElement("span");
+        const e_takeoff_location = document.createElement("span");
 
-    //return takeoffs;
+        e_takeoff_name.innerText = takeoff.name;
+        e_takeoff_description.innerText = takeoff.description;//.split(' ').slice(0, 10).join(' ').substring(0, 10 * 9) + " (...)";
+        e_takeoff_region.innerText = takeoff.region;
+        e_takeoff_location.innerText = `${takeoff.latitude}, ${takeoff.longitude}`;
+
+        e_takeoff_container.append(e_takeoff_name, e_takeoff_description, e_takeoff_region, e_takeoff_location);
+        e_results_content.append(e_takeoff_container);
+
+        return [takeoff, e_takeoff_container];
+    });
+
+    return out;
+}
+
+function handle_sorting(data) {
+    // Get HTML elements
+    const e_search = document.getElementById("search");
+
+    // Guard against missing elements
+    const required_elements = [e_search];
+    if (required_elements.includes(null)) throw new Error("missing HTML elements");
+
+    // Event handlers
+    e_search.addEventListener("input", async (e) => update(data, e.target.value), false);
+
+    // Sort function
+    async function update(data, search_text) {
+        for (let [takeoff, e_takeoff] of data) {
+            const formatText = (text) => text.toLowerCase().replaceAll(' ', '');
+            const checkMatch = (text, search) => formatText(text).includes(formatText(search));
+            const match = checkMatch(takeoff.name + takeoff.description + takeoff.region, search_text);
+
+            e_takeoff.setAttribute("match", match);
+        }
+    };
 }
